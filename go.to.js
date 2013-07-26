9497a6262327d4fffa5961e969f88401b5f2585f
Zack Pitts <folkstring@hotmail.com>
2013-07-26 02:47:31 -0700

    Update go.to.js
    Simplest form of go.to routing.

--- go.to.js
+++ go.to.js
@@ -1,1 +1,25 @@
-
+var go = function (routes, model) {
+  
+    return {
+    
+        to: function (route) {        
+            
+            routes[route](model);
+            return this;
+            
+        }, 
+        
+        route: function (route) {
+        
+            return routes[route];
+        
+        },
+        
+        model: function () {
+        
+            return model;
+        
+        }
+    
+    };
+};
